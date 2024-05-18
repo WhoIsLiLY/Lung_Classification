6ce53a23e6e41c6a1d5666e3861616e00362d651
WILLY HIMAWAN <s160422011@microsoft.ubaya.ac.id>
2024-05-18 20:59:58 +0700

    install react

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)  # Add this line to enable CORS for all origins
+    
     with app.app_context():
         from . import routes
         return app
